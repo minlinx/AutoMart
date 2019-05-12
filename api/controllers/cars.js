9a0e87564e5405592d18822f5682d37dd32bf26b
Xtremrules <oleleweprince@outlook.com>
2019-05-12 16:08:56 +0100

    updates cars controller

--- api/controllers/cars.js
+++ api/controllers/cars.js
@@ -1,18 +1,23 @@
-import cars from '../models/cars';
-class Cars {
-    static allNewAvailableCars(request, response) {
-        const {status, state} = request.query;
-        const newAvailableCars =  cars.filter((car) => car.status === status && car.state === state);
-        if (newAvailableCars.length > 0) {
-            response.status(200).json({
-                status: 200,
-                data: newAvailableCars
-            });
-        }
-        response.status(404).json({
-            status: 404,
-            message: 'Search Not Found'
+import carsDB from '../models/cars';
+const cars = (request, response) => {
+    const {status, state} = request.query;
+    if (status && state) {
+        const newAvailableCars =  carsDB.filter((car) => car.status === status && car.state === state);
+        response.status(200).json({
+            status: 200,
+            data: newAvailableCars
         });
     }
+    else if (status) {
+        const availableCars =  carsDB.filter((car) => car.status === status);
+        return response.status(200).json({
+            status: 200,
+            data: availableCars
+        });
+    }
+    response.status(404).json({
+        status: 404,
+        message: 'Search Not Found'
+    });
 }
-export default Cars;
+export default cars;
