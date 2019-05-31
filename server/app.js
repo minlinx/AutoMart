413de287300987a9c38f5605eb4c26b745253976
minlinx <minaproblemsolver@gmail.com>
2019-05-31 01:00:25 -0700

    handles undefined routes' errors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ import bodyParser from 'body-parser';
 import carsRoute from '../api/routes/cars';
 import usersRoute from '../api/routes/users';
 import ordersRoute from '../api/routes/orders';
+import apiError from '../middlewares/apiError';
 
+const { notFoundError, serverError} = apiError;
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,5 +14,6 @@ app.use(expressValidator());
 app.use('/api/v1/auth', usersRoute);
 app.use('/api/v1/car', carsRoute);
 app.use('/api/v1/order', ordersRoute);
+app.use(notFoundError, serverError);
 app.listen(port, () => console.log(`Should Be Listening On Port ${port}...`));
 export default app;
