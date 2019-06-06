0e827cc49aee8a098ee92fea7372cbe363bb8a36
Xtremrules <oleleweprince@outlook.com>
2019-06-06 12:39:06 +0100

    modifies cars controllers

--- api/routes/cars.js
+++ api/routes/cars.js
@@ -1,46 +1,35 @@
 import express from 'express';
-// import validator from '../../middlewares/getRouteHandler';
-// import imageParser from '../../middlewares/uploadImage';
+import validator from '../../middlewares/getRouteHandler';
+import imageParser from '../../middlewares/uploadImage';
 import Cars from '../controllers/cars';
 
-// const {
-// 	postCarAdValidator,
-// 	checkCarId,
-// 	checkStatus,
-// 	checkBodyType,
-// 	checkStatusAndState,
-// 	checkStatusMinPriceAndMaxPrice,
-// 	checkCarADStatus,
-// 	checkCarADPrice
-// } = validator;
 const {
-	// getAllCars,
-	// getCarsWithStatus,
-	// getCarsWithBodyType,
-	// getCarsWithStatusAndState,
-	getCarOrCars
-	// specificCar,
-	// postCarAd,
-	// deleteCarAd,
-	// changeCarAdPrice,
-	// changeCarAdStatus
+	postCarAdValidator,
+	checkCarId,
+	checkCarADStatus,
+	checkCarADPrice
+} = validator;
+const {
+	getCarOrCars,
+	specificCar,
+	postCarAd,
+	deleteCarAd,
+	changeCarAdPrice,
+	changeCarAdStatus
 } = Cars;
 const router = express.Router();
-// router.post('/status', checkStatus(), getCarsWithStatus);
-// router.post('/bodyType', checkBodyType(), getCarsWithBodyType);
 router.get(
 	'/',
 	getCarOrCars
 );
-// router.post('/statusAndState', checkStatusAndState(), getCarsWithStatusAndState);
-// router.get('/:carId', checkCarId(), specificCar);
-// router.post(
-// 	'/',
-// 	imageParser.single('carImage'),
-// 	postCarAdValidator(),
-// 	postCarAd
-// );
-// router.delete('/:carId', checkCarId(), deleteCarAd);
-// router.patch('/:carId/price', checkCarADPrice(), changeCarAdPrice);
-// router.patch('/:carId/status', checkCarADStatus(), changeCarAdStatus);
+router.get('/:carId', checkCarId(), specificCar);
+router.post(
+	'/',
+	imageParser.single('carImage'),
+	postCarAdValidator(),
+	postCarAd
+);
+router.delete('/:carId', checkCarId(), deleteCarAd);
+router.patch('/:carId/price', checkCarADPrice(), changeCarAdPrice);
+router.patch('/:carId/status', checkCarADStatus(), changeCarAdStatus);
 export default router;
