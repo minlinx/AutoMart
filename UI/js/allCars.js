4b8ce136e8f816965ae8e9b79ce5061cf9240589
Xtremrules <oleleweprince@outlook.com>
2019-07-02 13:02:13 +0100

    creates UI/allcars.html

---
+++ UI/js/allCars.js
@@ -0,0 +1,64 @@
+const createDivElementWithClassName = (className) => {
+	let div = document.createElement('div');
+	div.setAttribute('class', className);
+	return div;
+};
+const myInit = {
+	method: 'GET',
+	mode: 'cors',
+	cache: 'default',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+};
+const getAllCars = async () => {
+	try {
+		const response = await fetch('/api/v1/car', myInit);
+		if (response.ok) {
+			const serverResponseData = await response.json();
+			const { data } = serverResponseData;
+			data.forEach(car => {
+				const { car_image, manufacturer, price, state } = car;
+				const img = document.createElement('img');
+				const span = document.createElement('span');
+				const strong = document.createElement('strong');
+				let divallUnsoldCarsMainView = createDivElementWithClassName('all-unsold-cars-main-view');
+				let divCarAd = createDivElementWithClassName('car-ad');
+				let divCarAdImage = createDivElementWithClassName('car-ad-image');
+				let divCarAdDescription = createDivElementWithClassName('car-ad-description');
+				let divCarAdPrice = createDivElementWithClassName('car-ad-price');
+				let divCarAdBrand = createDivElementWithClassName('car-ad-brand');
+				let divCarAdState = createDivElementWithClassName('car-ad-state');
+				divCarAdState.appendChild(strong);
+				divCarAdImage.appendChild(img);
+				divCarAdState.appendChild(span);
+				divCarAdPrice.appendChild(strong);
+				divCarAdBrand.appendChild(span);
+				divCarAdDescription.appendChild(divCarAdState);
+				divCarAdDescription.appendChild(divCarAdPrice);
+				divCarAdDescription.appendChild(divCarAdBrand);
+				divCarAd.appendChild(divCarAdImage);
+				divCarAd.appendChild(divCarAdDescription);
+				divallUnsoldCarsMainView.appendChild(divCarAd);
+				divCarAdImage.firstChild.setAttribute('src', car_image);
+				divCarAdState.innerHTML = state;
+				divCarAdPrice.firstChild.innerHTML = price;
+				divCarAdBrand.firstChild.innerHTML = manufacturer;
+				divCarAdBrand.firstChild.style.display = 'block';
+				let div1 = document.querySelector('.all-unsold-cars-main-view');
+				div1.appendChild(divCarAd);
+				console.log(divCarAdState);
+				console.log(divCarAdPrice);
+				console.log(divCarAdBrand);
+				console.log(divCarAdImage);
+				console.log(car);
+			});
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	getAllCars();
+});
--- dbConifg.js
+++ dbConifg.js
@@ -1,33 +1,7 @@
-// import { Pool } from 'pg';
-// const pool = new Pool({
-// 	host: process.env.PGHOST,
-// 	database: process.env.PGDATABASE,
-// 	password: process.env.PGPASSWORD,
-// 	port: process.env.PGPORT,
-// });
-// export default pool;
-
-/////////////////////////// Using Client ////////////////////////////////////////////
-// import { Client } from 'pg';
-
-// const pool = new Client({
-// 	connectionString: process.env.PROD_DATABASE_URL,
-// 	ssl: false,
-// });
-// export default pool;
-
-/////////////////////////////  Usikng  Clientg ///////////////////////////////////////
-import pg from 'pg';
+import { pg, Pool } from 'pg';
 if (process.env.DATABASE_URL) {
 	pg.defaults.ssl = true;
 }
-import { Pool } from 'pg';
-
-// const defaultConfig = {
-// 	connectionString: process.env.DEV_DATABASE_URL,
-// 	ssl: true
-// };
-
 const productionConfig = {
 	connectionString: process.env.DATABASE_URL,
 	ssl: true
@@ -38,16 +12,6 @@ const testConfig = {
 	ssl: true
 };
 const getConnectionString = () => {
-	// switch (process.env.NODE_ENV) {
-	// case 'test':
-	// 	return testConfig;
-	// case 'production':
-	// 	return productionConfig;
-	// case 'development':
-	// 	return productionConfig;
-	// default:
-	// 	return productionConfig;
-	// }
 	if (process.env.NODE_ENV === 'test') {
 		return testConfig;
 	}
@@ -64,18 +28,3 @@ const getConnectionString = () => {
 const pool = new Pool(getConnectionString());
 
 export default pool;
-////////////////////////////////////////////////////////////////////////////
-// import pg from 'pg';
-// if (process.env.DATABASE_URL) {
-// 	pg.defaults.ssl = true;
-// }
-
-// // include an OR statement if you switch between a local dev db and
-// // a remote heroku environment
-// import { Pool } from 'pg';
-// let connString = process.env.DATABASE_URL;
-
-// const pool = new Pool({
-// 	connectionString : connString
-// });
-// export default pool;
