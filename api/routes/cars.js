ada658973ad2e14cf2d201667ffdfe78e577ff16
Xtremrules <oleleweprince@outlook.com>
2019-07-14 20:12:10 +0100

    modifies middlewares/checkUserAuthentication.js

--- api/routes/cars.js
+++ api/routes/cars.js
@@ -6,25 +6,25 @@ import Cars from '../controllers/cars';
 
 const {
 	postCarAdValidator,
-	// checkCarId,
+	checkCarId,
 	checkCarADStatus,
 	checkCarADPrice
 } = validator;
 const {
-	// getCarOrCars,
-	// specificCar,
+	getCarOrCars,
+	specificCar,
 	postCarAd,
-	// deleteCarAd,
+	deleteCarAd,
 	changeCarAdPrice,
 	changeCarAdStatus
 } = Cars;
 const router = express.Router();
-// router.get(
-// 	'/',
-// 	checkAuthentication,
-// 	getCarOrCars
-// );
-// router.get('/:car_id', checkAuthentication, specificCar);
+router.get(
+	'/',
+	checkAuthentication,
+	getCarOrCars
+);
+router.get('/:car_id', checkAuthentication, specificCar);
 router.post(
 	'/',
 	checkAuthentication,
@@ -32,7 +32,7 @@ router.post(
 	postCarAdValidator(),
 	postCarAd
 );
-// router.delete('/:car_id', checkCarId(), checkAuthentication, deleteCarAd);
+router.delete('/:car_id', checkCarId(), checkAuthentication, deleteCarAd);
 router.patch('/:car_id/price', checkCarADPrice(), checkAuthentication, changeCarAdPrice);
 router.patch('/:car_id/status', checkCarADStatus(), checkAuthentication, changeCarAdStatus);
 export default router;
