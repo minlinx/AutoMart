c86e4addc15ac5ba8bfeab84bd47e9f4de139d28
Xtremrules <oleleweprince@outlook.com>
2019-07-14 16:19:51 +0100

    modifies middlewares/checkUserAuthentication.js

--- middlewares/checkUserAuthentication.js
+++ middlewares/checkUserAuthentication.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 const privateKey = process.env.JWT_PRIVATE_KEY || 'automart';
 function checkUserAuthentication(request, response, next) {
+	console.log(request.headers);
 	const bearerToken = request.headers.authorization;
 	const { token } = request.body;
 	const headersToken = request.headers.token;
