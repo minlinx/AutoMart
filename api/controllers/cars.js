acb864b388d642cfd0eb6540d0b5fd5f5d56dcd9
Xtremrules <oleleweprince@outlook.com>
2019-07-15 14:38:11 +0100

    modifies middlewares/checkUserAuthentication.js

--- api/controllers/cars.js
+++ api/controllers/cars.js
@@ -44,7 +44,7 @@ class Cars {
 						});
 					}
 					else {
-						const data = { ...result.rows };
+						const data = [...result.rows];
 						return response.status(200).json({
 							status: 200,
 							data
@@ -55,7 +55,7 @@ class Cars {
 		else {
 			return response.status(400).json({
 				status: 400,
-				error: 'Bae Request'
+				error: 'Bad Request'
 			});
 		}
 		// else if (priceRange && queryLength === 3) {
@@ -374,7 +374,6 @@ class Cars {
 		// }
 	}
 	static async specificCar(request, response, next) {
-		console.log(request);
 		// const { token } = response.locals;
 		const queryLength = parseInt(Object.keys(request.query).length);
 		const { car_id } = request.params;
