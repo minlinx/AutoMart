0a4ddc9a394d61b600204a154022bfa01e0d7b53
Xtremrules <oleleweprince@outlook.com>
2019-07-16 19:11:01 +0100

    modifies middlewares/checkUserAuthentication.js

--- middlewares/checkUserAuthentication.js
+++ middlewares/checkUserAuthentication.js
@@ -1,20 +1,27 @@
 import jwt from 'jsonwebtoken';
 const privateKey = process.env.JWT_PRIVATE_KEY || 'automart';
 function checkUserAuthentication(request, response, next) {
-	const bearerToken = request.headers.bearerToken;
-	const token = bearerToken.split(' ')[1];
-	const headersToken = request.headers.token || request.body.token || token;
-	const decodedToken = jwt.verify(headersToken, privateKey);
-	if (!decodedToken) {
+	try {
+		const bearerToken = request.headers.bearerToken;
+		const token = bearerToken.split(' ')[1];
+		const headersToken = request.headers.token || request.body.token || token;
+		const decodedToken = jwt.verify(headersToken, privateKey);
+		if (!decodedToken) {
+			return response.status(401).json({
+				status: 401,
+				error: 'Unauthorized'
+			});
+		}
+		else {
+			request.user = decodedToken;
+			request.token = headersToken;
+			return next();
+		}
+	} catch (error) {
 		return response.status(401).json({
 			status: 401,
-			error: 'Unauthorized'
+			error: 'Authentication Failed'
 		});
-	}
-	else {
-		request.user = decodedToken;
-		request.token = headersToken;
-		return next();
 	}
 }
 export default checkUserAuthentication;
