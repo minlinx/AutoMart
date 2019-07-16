7ae5fa06036e94fff164ab5ed462ddc23c763e7d
Xtremrules <oleleweprince@outlook.com>
2019-07-16 22:37:19 +0100

    modifies middlewares/checkUserAuthentication.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import validator from '../../middlewares/getRouteHandler';
+// import validator from '../../middlewares/getRouteHandler';
 import checkAuthentication from '../../middlewares/checkUserAuthentication';
 import Orders from '../controllers/order';
 
-const { createOrderValidator, updateOrderValidator } = validator;
+// const { createOrderValidator, updateOrderValidator } = validator;
 const { createOrder, updateOrder } = Orders;
 const router = express.Router();
-router.post('/', checkAuthentication, createOrderValidator(), createOrder);
-router.patch('/:order_id/price', checkAuthentication, updateOrderValidator(), updateOrder);
+router.post('/', checkAuthentication, createOrder);
+router.patch('/:order_id/price', checkAuthentication, updateOrder);
 export default router;
