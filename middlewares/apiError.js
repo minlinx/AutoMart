5a74befada8f856c1fec190e719f1c1fb6797be8
Xtremrules <oleleweprince@outlook.com>
2019-07-19 15:44:29 +0100

    modifies api/controllers/cars.js

--- middlewares/apiError.js
+++ middlewares/apiError.js
@@ -1,20 +1,18 @@
 class apiEror {
 	static notFoundError(request, response, next) {
 		const error = new Error('Not Found, check the URL you typed and try again');
-		console.log('From apierrormiddleware', error);
 		error.status = 404 || 500;
 		return next(error);
 	}
 	static serverError(error, request, response, next) {
 		if (error.status === 404) {
-			console.log('From 404 apierrormiddleware', error);
 			return response.status(404).json({
 				status: 404,
 				error: error.message
 			});
 		}
 		else {
-			console.log('From apierrormiddleware', error);
+
 			return response.status(500).json({
 				status: 500,
 				error: 'Oops!!! Server is down, Try again later'
